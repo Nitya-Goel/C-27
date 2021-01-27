a1810cdc09f26aacb7f1e29d7b33456827705d9a
Nitya-Goel <nityagoel04@gmail.com>
2021-01-27 16:52:21 +0530

    added chain

---
+++ Chain.js
@@ -0,0 +1,17 @@
+class Chain{
+    constructor(body1, body2){
+        var options= {
+            bodyA: body1,
+            bodyB: body2,
+            stiffness: 1,
+            length: 25
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+    display(){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
+        line(pointA.x,pointA.y,pointB.x, pointB.y);  
+    }
+}
